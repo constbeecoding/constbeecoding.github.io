10e65d2547b8c75d53814c19496b89b3300e27da
Bee Kay <brittkcrouch@gmail.com>
2021-01-19 22:04:26 -0600

    Added new JS file, commented some portfolio ideas.

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+
+//Add a mouse over effect for moon/tarot cards etc???
+const moon = document.getElementsByClassName('moon');
+
+moon.onmouseover = moonSet;
+moon.onmouseout = moonRise;
+
+function moonSet {
+
+
+}
+
+function moonRise {
+
+
+}
+//loop slowly over luna letters
+//add dark mode on moon toggle?
